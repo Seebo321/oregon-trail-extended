a67f4728ab8559db0c1a49c43da6a2460f445b7d
Samuel Campbell <seebo321@comcast.net>
2020-07-16 16:28:43 -0500

    added hunter and doctor sublcasses

--- traveler.js
+++ traveler.js
@@ -18,6 +18,40 @@ class Traveler {
                      this.food-=1}
     }   
 }
+
+
+
+class Doctor extends Traveler{
+    constructor(name){
+    super(name)}
+    heal(patient) {
+        console.log('here lemme check you out '+ patient.name+'.')
+        patient.isHealthy=true}
+}
+
+class Hunter extends Traveler{
+    constructor(name){
+    super(name)
+    this.food=2
+}
+   hunt(){
+    console.log(this.name+' went out hunting and found 5 food!')   
+    this.food+=5}
+   eat(){if(this.food<2){
+    console.log(this.name + ' is out of food and now unhealthy')
+                this.food=0
+                this.isHealthy=false}
+        else{
+            console.log(this.name +' has eaten some food!')
+            this.food-=2}}
+    giveFood(hungrySap,numOfFoodUnits){
+        if(numOfFoodUnits>this.food){console.log('haha just kidding not enough food to give you')}else{
+        console.log(hungrySap.name+' you can have my left overs.')
+        this.food-=numOfFoodUnits
+        hungrySap.food+=numOfFoodUnits
+    }}
+
+}
                     // testing below
 // let joe= new traveler('joe')
 // console.log('test '+joe.food)
